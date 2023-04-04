10c774a9443da385e09d3b5177a4aa78f467708c
Alexander Baggett <alexander.baggett@gmail.com>
2023-04-04 11:56:45 -0500

    Added files

---
+++ FastFor.cs
@@ -0,0 +1,54 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace FastForloop
+{
+
+    public static class Loops
+    {
+
+        public static void FastFor<T>(T[] items, Action codeblock)
+        {
+            ref var start = ref MemoryMarshal.GetArrayDataReference(items);
+            ref var end = ref Unsafe.Add(ref start, items.Length);
+
+            while (Unsafe.IsAddressLessThan(ref start, ref end))
+            {
+                codeblock();
+            }
+        }
+
+        public static void SpanFor<T>(T[] items, Action codeblock)
+        {
+            var span = items.AsSpan();
+
+            for (int i = 0; i < span.Length; i++)
+            {
+                codeblock();
+            }
+        }
+    }
+    public static class ArrayExtensions
+    {
+        public static void FastFor<T>(this T[] items, Action codeblock)
+        {
+            ref var start = ref MemoryMarshal.GetArrayDataReference(items);
+            ref var end = ref Unsafe.Add(ref start, items.Length);
+
+            while (Unsafe.IsAddressLessThan(ref start, ref end))
+            {
+                codeblock();
+            }
+        }
+
+        public static void SpanFor<T>(this T[] items, Action codeblock)
+        {
+            var span = items.AsSpan();
+
+            for (int i = 0; i < span.Length; i++)
+            {
+                codeblock();
+            }
+        }
+    }
+}
